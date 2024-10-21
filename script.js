86a84a9654e40a06e8451205af88506ed7d36d80
I A <ilie.adrian1986@gmail.com>
2024-10-21 18:42:54 +0300

    css and html finished / basic function for slide buttons added

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
-console.log("Hello world")
+let slideIndex = 5;
+const slides = document.getElementsByClassName("slide");
+const leftArrow = document.getElementById("left-arrow");
+const rightArrow = document.getElementById("right-arrow");
+
+leftArrow.addEventListener("click", fireSlideDecrease);
+rightArrow.addEventListener("click", fireSlideIncrease);
+
+function fireSlideDecrease() {
+  if (slideIndex > 0) {
+    slideIndex--;
+    console.log(slideIndex);
+
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    slides[slideIndex].style.display = "block";
+  } else {
+    return;
+  }
+}
+
+function fireSlideIncrease() {
+  if (slideIndex < slides.length) {
+    slideIndex++;
+    console.log(slideIndex);
+    console.log(slides[slideIndex]);
+
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    slides[slideIndex].style.display = "block";
+  } else {
+    return;
+  }
+}
